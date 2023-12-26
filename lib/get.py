0125d82b01d48d581d77894deb2050ca996a2e9b
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-26 12:26:45 +0300

    updated to show the process I used to get my repl to work

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,16 @@
+python3
 import requests
-import json
-
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
-
+pip3 install requests
+url = "https://learn-co-curriculum.github.io/json-site-example/"
 response = requests.get(url)
+print(response.text)
 
+
+import requests
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+response = requests.get(url)
+print(response.content)
+import json
+json.loads(response.content)
 json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
+print(json.dumps(json_content, indent=4, sort_keys=True))
